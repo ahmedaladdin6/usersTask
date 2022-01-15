428cbe4530ead3208ea3fa51445b079a1f0ec8f1
Ahmed <aalaa@youxel.com>
2022-01-16 01:58:34 +0200

    push on repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import users from './files/users.json'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'usersTask';
+  title = 'userAssignment';
+
+  usersListData: { name: string, code: string }[] = users;
+  users = []
+  levels = [];
+
+  ngOnInit(): void {
+    this.initUsers(this.usersListData)
+  }
+
+  initUsers(usersData) {
+    usersData.map((item) => {
+      this.createUSersWithLevels(item)
+    })
+    this.levels = [...new Set(this.levels)]
+  }
+
+  createUSersWithLevels(item) {
+    const userListCode = item.code.split('.')
+    this.users.push({
+      code: userListCode[userListCode.length - 1],
+      level: userListCode.length,
+      name: item.name,
+      parent: userListCode[userListCode.length - 2]
+    })
+    this.levels.push(userListCode.length)
+  }
+
 }
